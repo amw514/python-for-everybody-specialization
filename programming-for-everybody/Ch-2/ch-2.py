758adc0d29f24f654d6779161107526e57c8932e
amw514 <alimunirwaris1@gmail.com>
2023-04-02 03:36:20 +0500

    Age Differnece

--- programming-for-everybody/Ch-2/ch-2.py
+++ programming-for-everybody/Ch-2/ch-2.py
@@ -4,3 +4,18 @@ hours = 30
 rate = 15
 pay = hours * rate
 print(rate)
+
+#Input in python
+
+name = input("Who are you? ")
+print("Welcome " + name)
+
+#Age difference between Ali and his classmate
+
+age = input("What is your age? ")
+age_diff = int(age) - 21
+if age_diff < 0:
+    positive_age = age_diff * -1
+    print("Ali's and your age difference is", positive_age)
+else:
+    print("Ali's and your age difference is", age_diff)
