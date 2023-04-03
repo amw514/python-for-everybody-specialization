0972a2f7e2e0b57a1dead10ae430da30a2a63d32
amw514 <alimunirwaris1@gmail.com>
2023-04-03 20:35:11 +0500

    Ch-5 Completed

--- programming-for-everybody/Ch-5/practice-ch-4.py
+++ programming-for-everybody/Ch-5/practice-ch-4.py
@@ -19,4 +19,46 @@ print("Blastoff!")
 friends = ['Tofeeq',"Hamza","Ali","Ahmad"]
 
 for friend in friends:
-    print("Hello dear "+friend)    
+    print("Hello dear "+friend)   
+    
+# Largest number so far 
+
+largest_so_far = -1
+numbers_list = [21,3,13,56,7,45,63,23,54,34]
+for num in numbers_list:
+    if num > largest_so_far:
+        largest_so_far = num
+    print(f"Largest number is {largest_so_far} and current number is {num}")
+print("Largest number", largest_so_far)
+
+# Sum in Loop
+
+zork = 0
+print("Before Sum = 0")
+for thing in numbers_list:
+    zork += thing
+    print(f"Sum is {zork} and current number is {thing}")
+print("Sum is", zork)
+
+# FInding the average in a loop
+
+count = 0
+sum = 0 
+print("Before FInding", count, sum)
+for thing in numbers_list:
+    count += 1
+    sum += thing
+    print(f"Sum is {sum} and current number is {thing} and count is {count}")
+print("Sum is", sum, "Average is", sum/count)
+
+# None value type
+
+smallest = None
+
+for thing in numbers_list:
+    if smallest == None:
+        smallest = thing
+    elif thing < smallest:
+        smallest = thing
+    print(f"Smallest is {smallest} and current number{thing}")
+print("Smallest is", smallest)
