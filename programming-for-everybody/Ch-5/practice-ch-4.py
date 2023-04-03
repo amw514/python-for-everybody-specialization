51c78911499ac169a684f2b986c9a1388445eb69
amw514 <alimunirwaris1@gmail.com>
2023-04-03 16:15:42 +0500

    CH-5 Practice

---
+++ programming-for-everybody/Ch-5/practice-ch-4.py
@@ -0,0 +1,22 @@
+# While loop
+
+n = 5
+while n  > 0:
+    print(n)
+    n-=1
+print("Blastoff!")
+
+
+# For Loop 
+#Definite Loop
+
+for i in [5,4,3,2,1,0]:
+    print(i)
+print("Blastoff!")
+
+# Another definite loop
+
+friends = ['Tofeeq',"Hamza","Ali","Ahmad"]
+
+for friend in friends:
+    print("Hello dear "+friend)    
