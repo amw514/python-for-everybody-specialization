dbb08e21a991bb21ec184484be764063c512523f
amw514 <alimunirwaris1@gmail.com>
2023-04-05 04:36:24 +0500

    Ch-7 Completed

---
+++ Ch-7/file1.py
@@ -0,0 +1,5 @@
+handling = open("file1.txt")
+count = 0
+for i in handling:
+    count += 0
+print("line count: ",count)
---
+++ Ch-7/filehandling.py
@@ -0,0 +1,7 @@
+handling = open("file1.txt")
+
+reading = handling.read()
+
+length = len(reading)
+
+print(length)
